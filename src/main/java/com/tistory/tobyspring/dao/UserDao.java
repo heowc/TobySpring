74f208af8a87da1382656c4f1ac09659acfe8522
heo won chul <heowc1992@gmail.com>
2017-06-13 12:25:47 +0900

    get() 예외 처리 및 test 코드 작성

--- src/main/java/com/tistory/tobyspring/dao/UserDao.java
+++ src/main/java/com/tistory/tobyspring/dao/UserDao.java
@@ -1,6 +1,7 @@
 package com.tistory.tobyspring.dao;
 
 import com.tistory.tobyspring.domain.User;
+import org.springframework.dao.EmptyResultDataAccessException;
 
 import javax.sql.DataSource;
 import java.sql.Connection;
@@ -48,15 +49,20 @@ public class UserDao {
         ps.setString(1, id);
 
         ResultSet rs = ps.executeQuery();
-        rs.next();
-        User user = new User();
-        user.setId(rs.getString("id"));
-        user.setName(rs.getString("name"));
-        user.setPassword(rs.getString("password"));
+        User user = null;
+
+        if( rs.next() ) {
+            user = new User();
+            user.setId(rs.getString("id"));
+            user.setName(rs.getString("name"));
+            user.setPassword(rs.getString("password"));
+        }
 
         rs.close();
         ps.close();
         c.close();
+
+        if (user == null) throw new EmptyResultDataAccessException(1);
 
         return user;
     }
--- src/test/java/com/tistory/tobyspring/dao/UserDaoTest.java
+++ src/test/java/com/tistory/tobyspring/dao/UserDaoTest.java
@@ -1,9 +1,11 @@
 package com.tistory.tobyspring.dao;
 
 import com.tistory.tobyspring.domain.User;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.dao.EmptyResultDataAccessException;
 
 import java.sql.SQLException;
 
@@ -12,14 +14,17 @@ import static org.junit.Assert.assertThat;
 
 public class UserDaoTest {
 
+    private static UserDao dao;
+
+    @BeforeClass
+    public static void beforeClass_init() throws SQLException {
+        ApplicationContext context = new ClassPathXmlApplicationContext("context-datasource.xml");
+        dao = context.getBean("userDao", UserDao.class);
+        dao.createTable();
+    }
+
     @Test
     public void test_addAndGet() throws SQLException {
-        ApplicationContext context = new ClassPathXmlApplicationContext("context-datasource.xml");
-        UserDao dao = context.getBean("userDao", UserDao.class);
-        dao.createTable();
-
-        /* ============================================================================ */
-
         dao.deleteAll();
         assertThat(dao.getCount(), is(0));
 
@@ -35,5 +40,32 @@ public class UserDaoTest {
 
         assertThat(user.getName(), is(user2.getName()));
         assertThat(user.getPassword(), is(user2.getPassword()));
+    }
+
+    @Test
+    public void test_count() throws SQLException {
+        User user1 = new User("wonchul", "허원철", "1234");
+        User user2 = new User("naeun", "전나은", "1234");
+        User user3 = new User("toby", "이일민", "1234");
+
+        dao.deleteAll();
+        assertThat(dao.getCount(), is(0));
+
+        dao.add(user1);
+        assertThat(dao.getCount(), is(1));
+
+        dao.add(user2);
+        assertThat(dao.getCount(), is(2));
+
+        dao.add(user3);
+        assertThat(dao.getCount(), is(3));
+    }
+
+    @Test(expected = EmptyResultDataAccessException.class)
+    public void test_getUserFailure() throws SQLException {
+        dao.deleteAll();
+        assertThat(dao.getCount(), is(0));
+
+        dao.get("unknown_id"); // Incorrect result size: expected 1, actual 0
     }
 }
