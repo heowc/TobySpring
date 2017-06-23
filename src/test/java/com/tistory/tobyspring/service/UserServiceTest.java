8a94e21cbb605b9e1d9732311a3f7f6b48329125
heo won chul <heowc1992@gmail.com>
2017-06-23 10:22:57 +0900

    다이나믹 프록시를 이용한 트랙잭션 적용

--- src/test/java/com/tistory/tobyspring/service/UserServiceTest.java
+++ src/test/java/com/tistory/tobyspring/service/UserServiceTest.java
@@ -15,7 +15,9 @@ import org.springframework.mail.MailSender;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.PlatformTransactionManager;
 
+import java.lang.reflect.Proxy;
 import java.util.Arrays;
 import java.util.List;
 
@@ -32,6 +34,9 @@ public class UserServiceTest {
 
     @Autowired
     private UserService userService;
+
+    @Autowired
+    private PlatformTransactionManager transactionManager;
 
     @Autowired
     private UserDao userDao;
@@ -155,8 +160,21 @@ public class UserServiceTest {
             userDao.add(user);
         }
 
+        TransactionHandler txHandler = new TransactionHandler();
+        txHandler.setTarget(userService);
+        txHandler.setTransactionManager(transactionManager);
+        txHandler.setPattern("upgradeLevels");
+
+        UserService txUserService =
+                (UserService) Proxy.newProxyInstance(
+                                        getClass().getClassLoader(),
+                                        new Class[] { UserService.class },
+                                        txHandler
+                                    );
+
         try {
-            userService.upgradeLevels();
+            txUserService.upgradeLevels();
+//            userService.upgradeLevels();
             fail("TestUserLevelUpgradePolicyException expected");
         } catch (TestUserLevelUpgradePolicyException e) {
         }
