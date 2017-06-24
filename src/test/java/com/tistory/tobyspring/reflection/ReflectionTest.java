a9a05923616ef64d56e61908a68e042ae3362153
heo won chul <heowc1992@gmail.com>
2017-06-24 18:00:52 +0900

    어드바이저 적용

--- src/test/java/com/tistory/tobyspring/reflection/ReflectionTest.java
+++ src/test/java/com/tistory/tobyspring/reflection/ReflectionTest.java
@@ -2,6 +2,8 @@ package com.tistory.tobyspring.reflection;
 
 import org.junit.Test;
 import org.springframework.aop.framework.ProxyFactoryBean;
+import org.springframework.aop.support.DefaultPointcutAdvisor;
+import org.springframework.aop.support.NameMatchMethodPointcut;
 
 import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
@@ -56,6 +58,19 @@ public class ReflectionTest {
         assertThat(hello.sayThankYou("WonChul"), is("THANK YOU WONCHUL"));
     }
 
+    /**
+     * Spring - Proxy Factory Bean </br>
+     * <pre>
+     * * Advice : 부가 기능을 제공하는 오브젝트
+     * * Pointcut : 메소드 선정 알고리즘
+     *
+     * ProxyFactoryBean(생성) -> 다이나믹 프록시
+     *                       -> 포인트컷(기능부가 대상 확인)
+     *                       -> 어드바이스(선정된 메소드 요청)
+     *                       -> 콜백
+     *                       -> 타킷오브젝트(위임)
+     * </pre>
+     */
     @Test
     public void test_springProxyFactoryBean() {
         ProxyFactoryBean proxyFactoryBean = new ProxyFactoryBean();
@@ -67,5 +82,25 @@ public class ReflectionTest {
         assertThat(hello.sayHello("WonChul"), is("HELLO WONCHUL"));
         assertThat(hello.sayHi("WonChul"), is("HI WONCHUL"));
         assertThat(hello.sayThankYou("WonChul"), is("THANK YOU WONCHUL"));
+    }
+
+    /**
+     * 어드바이저 = 포인트컷 + 어드바이서
+     */
+    @Test
+    public void test_springProxyFactoryBeanWithAdvisor() {
+        ProxyFactoryBean proxyFactoryBean = new ProxyFactoryBean();
+        proxyFactoryBean.setTarget(new HelloTarget()); // 타겟 설정
+
+        NameMatchMethodPointcut pointcut = new NameMatchMethodPointcut(); // 메소드명 포인트컷 설정 객체 초기화
+        pointcut.setMappedName("sayH*"); // sayH로 시작하는 메소드명
+
+        proxyFactoryBean.addAdvisor(new DefaultPointcutAdvisor(pointcut, new UppercaseAdvice())); // 어드바이저 설정
+
+        Hello hello = (Hello) proxyFactoryBean.getObject(); // 생성된 프록스 초기화
+
+        assertThat(hello.sayHello("WonChul"), is("HELLO WONCHUL"));
+        assertThat(hello.sayHi("WonChul"), is("HI WONCHUL"));
+        assertThat(hello.sayThankYou("WonChul"), is("Thank You WonChul")); // toUppercase() 안됨
     }
 }
