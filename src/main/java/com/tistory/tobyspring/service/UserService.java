94897b02c4519e6f4e53de5d42c93a94511013c2
heo won chul <heowc1992@gmail.com>
2017-06-28 12:04:04 +0900

    @Transactional 적용

--- src/main/java/com/tistory/tobyspring/service/UserService.java
+++ src/main/java/com/tistory/tobyspring/service/UserService.java
@@ -1,14 +1,18 @@
 package com.tistory.tobyspring.service;
 
 import com.tistory.tobyspring.domain.User;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
+@Transactional
 public interface UserService {
 
     void add(User user);
 
+    @Transactional(readOnly = true)
     User get(String id);
+    @Transactional(readOnly = true)
     List<User> getAll();
     void deleteAll();
     void update(User user);
